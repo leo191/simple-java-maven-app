7031074ee0e130bf8ec2adfae2581768664ccb58
Subhadip Banerjee <subhadipbanerjee.527@gmail.com>
2021-01-14 01:47:49 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello leo my fuckboy!";
+    private final String message = "Hello leo my boy!";
 
     public App() {}
 
